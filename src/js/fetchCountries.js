b0bb239b5b80d91ed9d3f029f64e51b0ea44deaa
GolovinD <golovin.dmytro@gmail.com>
2022-11-09 19:51:48 +0200

    fixed fetch

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,14 +1,12 @@
 fetchCountries = (name) => {
     const URL = `https://restcountries.com/v3.1/name/${name}/?fields=name,capital,languages,flags,population`;
-    return fetch(URL)
-        // return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,languages,flags,population`)
-        .then(response => {
+    return fetch(URL).then(response => {
         if (!response.ok) {
         throw new Error(response.status);
       }
         return response.json();
     });
 }
-
+// return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,languages,flags,population`)
 
 export { fetchCountries };
